a06c4f35d73ab97c8cc2bc03111ee5842213ee47
FezDero <fezdero@gmail.com>
2024-11-30 15:31:45 +0500

    Add files via upload

---
+++ module_1_4.py
@@ -0,0 +1,17 @@
+my_string = input('Здравствуй, товарищ ')
+print (my_string.count('', 1))
+
+my_string = input("Hello, comrade ")
+print (my_string.upper())
+print (my_string.lower())
+print (my_string.replace(" ", ''))
+print (my_string[0])
+print (my_string[-1])
+
+my_string = "Hello, comrade"
+print (my_string.upper())
+print (my_string.lower())
+print (my_string.replace(" ", ''))
+print (my_string[0])
+print (my_string[-1])
+
